6693b065b88df689eabb64f3e6e61c673410c2f2
Jack Brinkman <jack.brinkman@csiro.au>
2022-11-02 12:52:46 +1000

    Implemented Mantine & refactored theming to use it

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,36 @@
+import {
+  MantineProvider,
+  ColorSchemeProvider,
+  ColorScheme,
+} from '@mantine/core';
+import { useLocalStorage, useColorScheme } from '@mantine/hooks';
 import { AuthProvider } from 'react-oidc-context';
-import Routes from './Routes';
+
+// App-specific imports
 import config from 'helpers/config';
+import Routes from './Routes';
 
 function App() {
+  const [colourScheme, setColourScheme] = useLocalStorage<ColorScheme>({
+    key: 'app-colour-scheme',
+    defaultValue: useColorScheme(),
+    getInitialValueInEffect: true,
+  });
+
+  // Helper function for switching the colour scheme
+  const toggleColourScheme = (value?: ColorScheme) =>
+    setColourScheme(value || (colourScheme === 'dark' ? 'light' : 'dark'));
+
   return (
     <AuthProvider {...config.auth}>
-      <Routes />
+      <ColorSchemeProvider
+        colorScheme={colourScheme}
+        toggleColorScheme={toggleColourScheme}
+      >
+        <MantineProvider withGlobalStyles withNormalizeCSS>
+          <Routes />
+        </MantineProvider>
+      </ColorSchemeProvider>
     </AuthProvider>
   );
 }
--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,101 +1,27 @@
+import { MantineThemeOverride } from '@mantine/core';
 import palette from './palette';
 
-interface Theme {
-  type: 'light' | 'dark';
-  radius: number;
-  skeleton: string;
-  defaults: {
-    viewPadding: number;
-  };
-  font: {
-    header: string;
-    body: string;
-    button: string;
-    chip: string;
-  };
-  background: ThemeColour;
-  text: ThemeColour;
-  colour: ThemeColour;
-  button: ThemeColour;
-  chip: ThemeColour;
-}
+const fontStack = `-apple-system, BlinkMacSystemFont, 'Helvetica Neue', 'Segoe UI', Oxygen, Ubuntu, sans-serif`;
+const defaults: MantineThemeOverride = {
+  fontFamily: `Roboto, ${fontStack}`,
+  headings: {
+    fontFamily: `Lato, ${fontStack}`,
+  },
+  defaultRadius: 'md',
+  // colors: {
+  //   test: [],
+  // },
+  // primaryColor: 'test',
+};
 
-interface ThemeColour {
-  primary: string;
-  secondary: string;
-  tertiary?: string;
-}
-
-const themes: { [key: string]: Theme } = {
+const themes: { [key: string]: MantineThemeOverride } = {
   light: {
-    type: 'light',
-    radius: 12,
-    skeleton: '#ecebeb',
-    defaults: {
-      viewPadding: 20,
-    },
-    font: {
-      header: 'Lato',
-      body: 'RobotoRegular',
-      button: 'RobotoBold',
-      chip: 'RobotoBold',
-    },
-    background: {
-      primary: '#FFFFFF',
-      secondary: palette.secondary.concrete,
-      tertiary: '#cecece',
-    },
-    text: {
-      primary: palette.secondary.charcoal,
-      secondary: palette.primary.grey,
-    },
-    colour: {
-      primary: palette.primary.flamingo,
-      secondary: palette.primary.rust,
-    },
-    button: {
-      primary: palette.primary.flamingo,
-      secondary: palette.primary.rust,
-    },
-    chip: {
-      primary: palette.extended.paleMoss,
-      secondary: palette.extended.plum,
-    },
+    ...defaults,
+    colorScheme: 'light',
   },
   dark: {
-    type: 'dark',
-    radius: 12,
-    skeleton: '#646464',
-    font: {
-      header: 'Lato',
-      body: 'RobotoRegular',
-      button: 'RobotoBold',
-      chip: 'RobotoBold',
-    },
-    defaults: {
-      viewPadding: 20,
-    },
-    background: {
-      primary: palette.secondary.charcoal,
-      secondary: '#373737',
-      tertiary: '#555555',
-    },
-    text: {
-      primary: '#FFFFFF',
-      secondary: palette.secondary.silver,
-    },
-    colour: {
-      primary: palette.primary.flamingo,
-      secondary: palette.primary.rust,
-    },
-    button: {
-      primary: palette.primary.flamingo,
-      secondary: palette.primary.rust,
-    },
-    chip: {
-      primary: palette.extended.paleMoss,
-      secondary: palette.extended.plum,
-    },
+    ...defaults,
+    colorScheme: 'dark',
   },
 };
 
