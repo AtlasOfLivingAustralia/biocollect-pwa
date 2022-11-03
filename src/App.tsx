4a2c0469f8209a15ef09f04ba9c2184491fa984b
Jack Brinkman <jack.brinkman@csiro.au>
2022-11-03 11:41:55 +1000

    Re-structured app to separate rendering of providers and main application component

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,33 @@
-import {
-  MantineProvider,
-  ColorSchemeProvider,
-  ColorScheme,
-} from '@mantine/core';
-import { useLocalStorage, useColorScheme } from '@mantine/hooks';
-import { AuthProvider } from 'react-oidc-context';
+import { useEffect } from 'react';
+import { useAuth } from 'react-oidc-context';
 
 // App-specific imports
-import config from 'helpers/config';
 import Routes from './Routes';
 import Layout from 'views/Layout';
-import { themes } from 'theme';
 
 function App() {
-  const [colourScheme, setColourScheme] = useLocalStorage<ColorScheme>({
-    key: 'app-colour-scheme',
-    defaultValue: useColorScheme(),
-    getInitialValueInEffect: true,
-  });
+  const auth = useAuth();
 
-  // Helper function for switching the colour scheme
-  const toggleColourScheme = (value?: ColorScheme) =>
-    setColourScheme(value || (colourScheme === 'dark' ? 'light' : 'dark'));
-
-  const onSigninCallback = () => {
+  // Effect hook to remove authentication search params from the URL
+  useEffect(() => {
     const params = new URLSearchParams(window.location.search);
-    if (params.get('code') && params.get('state')) {
+    if (params.get('code') && params.get('state') && auth.isAuthenticated) {
       params.delete('code');
       params.delete('state');
-    }
 
-    // Remove the auth code & state variables from the history
-    window.history.replaceState(
-      null,
-      '',
-      window.location.origin + window.location.pathname + params.toString()
-    );
-  };
+      // Remove the auth code & state variables from the history
+      window.history.replaceState(
+        null,
+        '',
+        window.location.origin + window.location.pathname + params.toString()
+      );
+    }
+  }, [auth.isAuthenticated]);
 
   return (
-    <AuthProvider {...config.auth} onSigninCallback={onSigninCallback}>
-      <ColorSchemeProvider
-        colorScheme={colourScheme}
-        toggleColorScheme={toggleColourScheme}
-      >
-        <MantineProvider
-          theme={themes[colourScheme]}
-          withGlobalStyles
-          withNormalizeCSS
-        >
-          <Layout>
-            <Routes />
-          </Layout>
-        </MantineProvider>
-      </ColorSchemeProvider>
-    </AuthProvider>
+    <Layout>
+      <Routes />
+    </Layout>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
+import {
+  MantineProvider,
+  ColorSchemeProvider,
+  ColorScheme,
+} from '@mantine/core';
+import { useLocalStorage, useColorScheme } from '@mantine/hooks';
+import { AuthProvider } from 'react-oidc-context';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// App-specific imports
+import App from './App';
+import config from 'helpers/config';
+import { themes } from 'theme';
+
+function Main() {
+  const [colourScheme, setColourScheme] = useLocalStorage<ColorScheme>({
+    key: 'app-colour-scheme',
+    defaultValue: useColorScheme(),
+    getInitialValueInEffect: true,
+  });
+
+  // Helper function for switching the colour scheme
+  const toggleColourScheme = (value?: ColorScheme) =>
+    setColourScheme(value || (colourScheme === 'dark' ? 'light' : 'dark'));
+
+  return (
+    <AuthProvider {...config.auth}>
+      <ColorSchemeProvider
+        colorScheme={colourScheme}
+        toggleColorScheme={toggleColourScheme}
+      >
+        <MantineProvider
+          theme={themes[colourScheme]}
+          withGlobalStyles
+          withNormalizeCSS
+        >
+          <App />
+        </MantineProvider>
+      </ColorSchemeProvider>
+    </AuthProvider>
+  );
+}
+
+export default Main;
