0e9512bb665de294220e2630605824c8a4324c4f
Jack Brinkman <jack.brinkman@csiro.au>
2023-08-17 16:03:43 +1000

    Added showUnpublishedRecords query parameter that shows the unpublished records dialog when the application loads

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,36 @@ import { Center, Loader } from '@mantine/core';
 
 // App-specific imports
 import Routes from './Routes';
+import { useContext, useEffect } from 'react';
+import { FrameContext } from 'helpers/frame';
 
 function App() {
   const { isLoading, isAuthenticated } = useAuth();
+  const frame = useContext(FrameContext);
+
   console.log(
     `[App] isLoading: ${isLoading} | isAuthenticated: ${isAuthenticated}`
   );
+
+  // Check for showUnpublishedRecords flag
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    if (params.get('showUnpublishedRecords') === 'true') {
+      // Open the unpublished records dialog
+      frame.open(
+        `${import.meta.env.VITE_API_BIOCOLLECT}/pwa/offlineList`,
+        'Unpublished Records'
+      );
+
+      // Remove the query param
+      params.delete('showUnpublishedRecords');
+      window.history.replaceState(
+        null,
+        '',
+        window.location.origin + window.location.pathname + params.toString()
+      );
+    }
+  }, []);
 
   if (isLoading) {
     return (
