b8cb80a0d00f9ca82693ab596c88b406f9f26002
Jack Brinkman <jack.brinkman@csiro.au>
2023-10-06 11:00:54 +1000

    Updated needsReauth helper to work with multiple auth providers

--- src/helpers/funcs/needsReauth.ts
+++ src/helpers/funcs/needsReauth.ts
@@ -1,8 +1,9 @@
 const EXPIRE_TIME = Math.pow(10, 12) * 2;
 
 const getStoredState = () => {
-  const [storeKey, storeState] = Object.entries(localStorage).find(([key]) =>
-    key.startsWith('oidc.user')
+  const { VITE_AUTH_AUTHORITY, VITE_AUTH_CLIENT_ID } = import.meta.env;
+  const [storeKey, storeState] = Object.entries(localStorage).find(
+    ([key]) => key === `oidc.user:${VITE_AUTH_AUTHORITY}:${VITE_AUTH_CLIENT_ID}`
   ) || [null, null];
 
   // If a user exists in the store, return the parsed JSON, otherwise return null
