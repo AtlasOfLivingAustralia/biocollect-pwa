ffc05893ef0c14d82ffe12874f00c8426f803b68
Jack Brinkman <jack.brinkman@csiro.au>
2024-11-28 13:45:05 +1000

    Removed release trigger & artifact uploading

--- cicd/build.sh
+++ cicd/build.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-tsc && for mode in production staging testing development; do
+tsc && for mode in prod staging test dev; do
   outDir="./dist/app-$mode"
   zipFile="./dist/app-$mode.zip"
   
   # If the mode is production, you may not need to specify the --mode flag
-  if [ "$mode" = "production" ]; then
+  if [ "$mode" = "prod" ]; then
     vite build --outDir "$outDir"
   else
     vite build --mode "$mode" --outDir "$outDir"
