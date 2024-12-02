ddaa60db3d75c0a79d834215ac7ceaf0ff9afd14
Jack Brinkman <jack.brinkman@csiro.au>
2024-12-02 11:45:51 +1000

    Update build.sh

--- cicd/build.sh
+++ cicd/build.sh
@@ -2,8 +2,7 @@
 
 tsc && for mode in prod staging test dev; do
   outDir="./dist/app-$mode"
-  zipFile="./dist/app-$mode.zip"
-  
+
   # If the mode is production, you may not need to specify the --mode flag
   if [ "$mode" = "prod" ]; then
     vite build --outDir "$outDir"
@@ -11,5 +10,7 @@ tsc && for mode in prod staging test dev; do
     vite build --mode "$mode" --outDir "$outDir"
   fi
   
-  zip -r "$zipFile" "$outDir"
+  cd "./dist/app-$mode"
+  zip -r "../app-$mode.zip" . *
+  cd ../..
 done
